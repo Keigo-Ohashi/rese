7b02122e5a4ff4e62ac6d4e8af338a6372670fdd
Ohashi Keigo <ex0084k@gmail.com>
2024-01-03 17:08:41 +0900

    データテーブル生成用のマイグレーションファイルの作成
    Changes to be committed:
    	modified:   src/database/migrations/2014_10_12_000000_create_users_table.php
    	new file:   src/database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
    	new file:   src/database/migrations/2023_12_29_171012_create_areas_table.php
    	new file:   src/database/migrations/2023_12_29_171022_create_genres_table.php
    	new file:   src/database/migrations/2023_12_29_171322_create_shops_table.php
    	new file:   src/database/migrations/2023_12_29_171332_create_reserves_table.php
    	new file:   src/database/migrations/2023_12_29_171400_create_likes_table.php

--- src/database/migrations/2023_12_29_171332_create_reserves_table.php
+++ src/database/migrations/2023_12_29_171332_create_reserves_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateReservesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reserves', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('shop_id')->constrained('shops')->onDelete('cascade');
+            $table->dateTime('reserve_date_time');
+            $table->unsignedInteger('reserve_num_people');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reserves');
     }
 }
--- src/database/migrations/2023_12_29_171400_create_likes_table.php
+++ src/database/migrations/2023_12_29_171400_create_likes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateLikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('shop_id')->constrained('shops')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('likes');
     }
 }

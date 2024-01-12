d36e935c2594acb647ea16f2211646c8f6c9f0ef
Ohashi Keigo <ex0084k@gmail.com>
2024-01-12 23:10:37 +0900

    都道府県のseederを作成
    Changes to be committed:
    	new file:   src/database/seeders/AreaSeeder.php
    	modified:   src/database/seeders/DatabaseSeeder.php

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,9 +3,14 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\AreaSeeder;
 
 class DatabaseSeeder extends Seeder
 {
+    private const SEEDERS = [
+        AreaSeeder::class,
+    ];
+
     /**
      * Seed the application's database.
      *
@@ -14,5 +19,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        foreach (self::SEEDERS as $seeder) {
+            $this->call($seeder);
+        }
     }
 }

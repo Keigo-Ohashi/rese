c8c6a7e81ec74d077c6f2d9b49bbbac2437320d9
Ohashi Keigo <ex0084k@gmail.com>
2024-02-09 00:15:32 +0900

    403・404ページを実装
    Changes to be committed:
    	new file:   src/resources/views/errors/403.blade.php
    	new file:   src/resources/views/errors/404.blade.php

---
+++ src/resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.message')
+
+@section('title')
+  <title>ページが見つかりませんでした</title>
+@endsection
+
+@section('message')
+  ページが見つかりませんでした
+@endsection
+
+@section('link')
+  <a href="/">トップへ</a>
+@endsection
